06776c4697be4f70baf6a14c284605817ac0d284
Amber <ahmborr@gmail.com>
2025-03-25 19:25:06 -0700

    import bcrypt

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,6 +1,7 @@
 import express from "express";
 import User from "../models/User.js";
 import crypto from "crypto";
+import bcrypt from "bcrypt";
 import { sendMail } from "../utils/sendMail.js";
 
 const router = express.Router();
